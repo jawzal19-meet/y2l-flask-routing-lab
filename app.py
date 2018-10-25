a4718b13de3d53f76d391b43e9c8842dc9509380
aboud20-meet <aboud20@meet.mit.edu>
2018-10-25 18:49:32 +0300

    I learned how to fork correctly now

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "You are home!"
+    return render_template("home.html")
+
+@app.route('/product')
+def product_page():
+    return render_template("product.html")
+
+@app.route('/shop')
+def shop_page():
+	products=["icecream", "chocolate", "candy"]
+	return render_template("shop.html", products=products)
 
 if __name__ == '__main__':
    app.run(debug = True)
